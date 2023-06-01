f38b3902d7bdefaecd6e668168dee22b8e865ee4
Hepheir <hepheir@gmail.com>
2023-06-01 21:15:39 +0900

    test: successful sensor but failed serial

--- ssg_bed_sensor.ino
+++ ssg_bed_sensor.ino
@@ -1,21 +1,53 @@
 #include "motion.h"
+#include "serial.h"
 
-Motion motion = Motion();
+Motion *motion;
+SerialHandler *serial_handler;
 
 void setup() {
+  SoftwareSerialBuilder *builder;
+  SoftwareSerial *serial;
+
+  motion = new Motion();
+  motion->setDataPin(2);
+  motion->setDuration(10000);
+
+  builder = new SoftwareSerialBuilder();
+  builder->setRxPin(3);
+  builder->setTxPin(4);
+  builder->setBaudRate(9600);
+
+  // serial = builder->buildAndListen();
+
+  serial = new SoftwareSerial(3, 4);
+
+  serial_handler = new SerialHandler(serial);
+
   Serial.begin(9600);
-
-  motion.setDataPin(2);
-  motion.setDuration(10000);
-
-  // LED Settings
-  pinMode(3, OUTPUT);
-  pinMode(8, OUTPUT);
 }
 
 void loop() {
-  motion.update();
+  motion->update();
 
-  digitalWrite(3, !motion.isInSleep());
-  digitalWrite(8, motion.isMotion());
+  if (motion->isInSleep()) {
+    serial_handler->transmit("INSLEEP=TRUE");
+    Serial.println("[transmitted] insleep: true");
+  } else {
+    serial_handler->transmit("INSLEEP=FALSE");
+    Serial.println("[transmitted] insleep: false");
+  }
+
+  if (motion->isMotion()) {
+    Serial.println("[transmitted] moving: true");
+  } else {
+    Serial.println("[transmitted] moving: false");
+  }
+
+  delay(200);
+
+  if (Serial.available()) {
+    while (Serial.available()) {
+      serial_handler->transmit(Serial.read());
+    }
+  }
 }
